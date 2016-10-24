d0933b5c69d7e7b9c4fe9b26fac8856892b4789a
Daniel Baker <dbaker@vistaprint.com>
2016-10-24 16:35:18 +0200

    Support empty out path. Only create target if it doesn't exist

--- main.rb
+++ main.rb
@@ -8,12 +8,25 @@ Zip.setup do |c|
     c.default_compression = Zlib::BEST_COMPRESSION
 end
 
+def directory_exists?(directory)
+  File.directory?(directory)
+end
+
 source = ARGV[0]
 target = ARGV[1]
 
 directoryToZip = Pathname.new(source)
-outputDir = Pathname.new(target + Socket.gethostname + "/" + directoryToZip.to_s().tr(':', ''))
-FileUtils.mkpath(outputDir.parent.to_s)
+
+if target.to_s == ''
+    outputDir = Pathname.new(directoryToZip.to_s())
+else     
+    outputDir = Pathname.new(target + Socket.gethostname + "/" + directoryToZip.to_s().tr(':', ''))
+end
+
+if(!directory_exists(outputDir.parent.to_s))
+    FileUtils.mkpath(outputDir.parent.to_s)
+end
+
 outputFile =  outputDir.to_s + "_" + Time.now.strftime("%Y%m%d%H%M%S") + ".zip"
 zf = ZipFileGenerator.new(directoryToZip, outputFile)
 zf.write()
--- src/backup.rb
+++ src/backup.rb
@@ -8,10 +8,25 @@ Zip.setup do |c|
     c.default_compression = Zlib::BEST_COMPRESSION
 end
 
-target = "J:/Back Up/"
-directoryToZip = Pathname.new("C:/Users/dbaker/Dropbox/Photos")
-outputDir = Pathname.new(target + Socket.gethostname + "/" + directoryToZip.to_s().tr(':', ''))
-FileUtils.mkpath(outputDir.parent.to_s)
+def directory_exists?(directory)
+  File.directory?(directory)
+end
+
+source = ARGV[0]
+target = ARGV[1]
+
+directoryToZip = Pathname.new(source)
+
+if target.to_s == ''
+    outputDir = Pathname.new(directoryToZip.to_s())
+else     
+    outputDir = Pathname.new(target + Socket.gethostname + "/" + directoryToZip.to_s().tr(':', ''))
+end
+
+if(!directory_exists?(outputDir.parent.to_s))
+    FileUtils.mkpath(outputDir.parent.to_s)
+end
+
 outputFile =  outputDir.to_s + "_" + Time.now.strftime("%Y%m%d%H%M%S") + ".zip"
 zf = ZipFileGenerator.new(directoryToZip, outputFile)
 zf.write()
